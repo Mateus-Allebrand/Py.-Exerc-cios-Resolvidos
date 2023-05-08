cb270bcc5a8810a17e8a2fd6ffb36c91e1e8ed6e
Mateus <mat.allebrand@gmail.com>
2023-05-08 14:44:18 -0300

    ex091 concluído

---
+++ world03.py/ex091.py
@@ -0,0 +1,27 @@
+#crie um programa onde 4 jogadores joguem um dado e tenham resultados aleátórios
+#guarde esses resultados em um dicionários e
+# no final , coloque esse dicionário em ordem , sabendo que o vencedor tirou o maior número no dado
+
+from random import randint
+from operator import itemgetter, attrgetter
+from time import sleep
+cont = 0
+jogt = {}
+jogadores = dict()
+
+print('-=' *30 
+      'jogo de dados')
+for c in range(0,4):
+    jogt['Jogador_01'] = randint(1,6)
+    jogt['Jogador_02'] = randint(1,6)
+    jogt['Jogador_03'] = randint(1,6)
+    jogt['Jogador_04'] = randint(1,6)
+    
+for i,j in jogt.items():
+    print(f'O {i} tirou o número {j}')
+    sleep(1)
+print('-=' *30)
+for d in sorted(jogt,key=jogt.get,reverse=True):
+    cont +=1
+    print(f'O {d} ficou em {cont}° lugar - {jogt[d]}')
+    sleep(1)
