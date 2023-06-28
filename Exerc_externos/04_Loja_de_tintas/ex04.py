a8328e5efdd6e07b0683f3b99e7c1aee9dc2d4d2
Mateus <mat.allebrand@gmail.com>
2023-06-28 12:00:56 -0300

    create ex

---
+++ Exerc_externos/04_Loja_de_tintas/ex04.py
@@ -0,0 +1,6 @@
+# Programa para loja de tintas:
+# O programa deve pedir o tamanho da area a ser pintada 
+# Deve calcular levando em consideração que um litro de tinta pinta 3 m² 
+# A tinta é vendida em latas de 1 litro , 3,6 litros e 18 litros.
+# ajude o cliente a tomar a melhor decisão 
+# o valor da lata de 18 litros é de 178,90 
