beb278850e8f8128d55568ce6636c3b3d9695e39
Mateus <mat.allebrand@gmail.com>
2023-07-05 10:14:06 -0300

    update

--- Data_science_Academy/Projeto_01/jogo_forca.py
+++ Data_science_Academy/Projeto_01/jogo_forca.py
@@ -1,18 +1,40 @@
 from random import choice
+from os import system, name
 
-#Definindo lista com todas as palavras possíveis
-possible_words = ["laranja", "abacate", "calabresa", "pizza", 'churrasco', 'manteiga', 'queijo', 'alface', 'legumes', 'cenora', 'espinafre', 'tomate', 'cebola']
 
-#lista vazia para receber letras adivinhadas
-empty_list = []
+# Funçao para limpar tela após cada execução
+def limpa_tela():
 
-#número maximo de tentativas = 
-nu_tentativas = 6
-cont_tentativas = 0
+    #windowns
+    if name == 'nt': #'nt' é o nome interno do windowns
+        _ = system('cls') #comando para limpar tela no windowns 
+        #Obs. _= é pq a função system retona algo. Estou jogando esse retorno para o (_)
+    #Mac ou linux
+    else:
+        _= system('clear')  #comando para limpar tela no Mac ou linux
 
-#Escolher uma palavra aleatória da lista
-chosen_word = choice(possible_words)
 
-while cont_tentativas < nu_tentativas:
-    cont_tentativas += 1
-    print(cont_tentativas)
+
+def game():
+    limpa_tela()
+    print("=-="*10)
+    print("JOGO DA FORCA".center(30))
+    print("=-="*10)
+    
+    print("Tente advinhar!".center(30))
+    print("")
+
+    #Definindo lista com todas as palavras possíveis
+    palavras = ["laranja", "abacate", "calabresa", "pizza", 'churrasco', 'manteiga', 'queijo', 'alface', 'legumes', 'cenora', 'espinafre', 'tomate', 'cebola']
+
+    #escolha palavras de modo randomico
+    palavra = choice(palavras)
+
+    n = len(palavra)
+
+    print("_ " * n)
+    print(palavra)
+    print(n)
+    
+
+game()
