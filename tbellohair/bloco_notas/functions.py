02fdd3096168362c564c340de883bcbcb6697b50
Mateus <mat.allebrand@gmail.com>
2023-08-09 11:36:20 -0300

    update

--- tbellohair/bloco_notas/functions.py
+++ tbellohair/bloco_notas/functions.py
@@ -1,24 +1,34 @@
 #Funções
 
+import datetime
+
 #Função linha dupla
 def rowdoble():
     return "=="*30
 
 
-def tabelaservcabelo(n):
-    servalores = [{"Corte": 30},
+def cabelo(n):
+    servalores = [0,{"Corte": 35},
                   {"Definitiva": 200},
                   {"Progressiova": 150},
                   {"Mechas": 120},
-                  {"Sair": 'Break'},
+                  
                   ]
     return servalores[n]
 
-def tabelaservunhas(n):
-    servalores = [{"Mão": 15},
+def unhas(n):
+    servalores = [0, {"Mão": 15},
                   {"Pé": 25},
                   {"Mão e Pé": 35},
-                  {"Sair": 'Break'},
+                  ]
+    return servalores[n]
+
+
+def sombrancelha(n):
+    servalores = [0,{"Rena": 25},
+                  {"Fio a Fio": 350},
+                  {"Tracional": 35},
+                  
                   ]
     return servalores[n]
 
@@ -29,49 +39,84 @@ def tiposerv():
         print(rowdoble())
         print("TIPO SERVIÇO:".center(60))
         print(rowdoble())
-        op = int(input("[[1]] Cabelo; \n\n[[2]] Sombrancelha; \n\n[[3]] Unha; \n\n[[0]] sair;" ))
+        op = int(input(f"[[1]] Cabelo; \n\n[[2]] Sombrancelha; \n\n[[3]] Unha; \n\n[[0]] sair; \n\n{rowdoble()}\n" ))
         while op not in range(0,4):
             print("\nOpção inválida! \n\n")
-            op = int(input("[[1]] Cabelo; \n\n[[2]] Sombrancelha; \n\n[[3]] Unha; \n\n[[0]] sair;" ))
+            op = int(input(f"[[1]] Cabelo; \n\n[[2]] Sombrancelha; \n\n[[3]] Unha; \n\n[[0]] sair; \n\n{rowdoble()}\n" ))
         if op == 1:
             print(rowdoble())
-            print("TIPO SERVIÇO PARA CABELO:".center(60))
+            print("CABELO:".center(60))
             print(rowdoble())
 
-            ops = int(input("[[1]] Corte; \n\n[[2]] Definitiva; \n\n[[3]] Mechas; \n\n[[4]] Progressiva; \n\n[[0]] sair;" ))
+            ops = int(input(f"[[1]] Corte; \n\n[[2]] Definitiva; \n\n[[3]] Mechas; \n\n[[4]] Progressiva; \n\n[[0]] sair; \n\n{rowdoble()}\n" ))
             while ops not in range(0,5):
                 print("\nOpção inválida! \n\n")
-                ops = int(input("[[1]] Corte; \n\n[[2]] Definitiva; \n\n[[3]] Mechas; \n\n[[4]] Progressiva; \n\n[[0]] sair;" ))
+                ops = int(input(f"[[1]] Corte; \n\n[[2]] Definitiva; \n\n[[3]] Mechas; \n\n[[4]] Progressiva; \n\n[[0]] sair; \n\n{rowdoble()}\n" ))
 
-                if  ops == 1:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 2:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 3:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 4:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 0:
-                    break
+            if  ops == 1:
+                listaserviços.append(cabelo(ops))
+            if  ops == 2:
+                listaserviços.append(cabelo(ops))
+            if  ops == 3:
+                listaserviços.append(cabelo(ops))
+            if  ops == 4:
+                listaserviços.append(cabelo(ops))
+            if  ops == 0:
+                break
+            break
             
+        
+
         if op == 2:
             print(rowdoble())
-            print("TIPO SERVIÇO PARA UNHAS:".center(60))
+            print("SOMBRANCELHA: ".center(60))
             print(rowdoble())
 
-            ops = int(input("[[1]] Mão; \n\n[[2]] Pé; \n\n[[3]] Mão e Pé; \n\n[[0]] sair;" ))
+            ops = int(input(f"[[1]] Rena; \n\n[[2]] Fio a Fio; \n\n[[3]] Tracional; \n\n[[0]] sair; \n\n{rowdoble()} \n" ))
             while ops not in range(0,4):
                 print("\nOpção inválida! \n\n")
-                ops = int(input("[[1]] Mão; \n\n[[2]] Pé; \n\n[[3]] Mão e Pé; \n\n[[0]] sair;" ))
+                ops = int(input(f"[[1]] Rena; \n\n[[2]] Fio a Fio; \n\n[[3]] Tracional; \n\n[[0]] sair; \n\n{rowdoble()} \n" ))
 
-                if  ops == 1:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 2:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 3:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 4:
-                    listaserviços.append(tabelaservcabelo(ops))
-                if  ops == 0:
-                    break
+            if  ops == 1:
+                listaserviços.append(sombrancelha(ops))
+            if  ops == 2:
+                listaserviços.append(sombrancelha(ops))
+            if  ops == 3:
+                listaserviços.append(sombrancelha(ops))
+            if  ops == 4:
+                listaserviços.append(sombrancelha(ops))
+            if  ops == 0:
+                break
+            break
+
+        if op == 3:
+            print(rowdoble())
+            print("UNHAS: ".center(60))
+            print(rowdoble())
+
+            ops = int(input(f"[[1]] Mão; \n\n[[2]] Pé; \n\n[[3]] Mão e Pé; \n\n[[0]] sair; \n\n{rowdoble()}\n" ))
+            while ops not in range(0,4):
+                print("\nOpção inválida! \n\n")
+                ops = int(input(f"[[1]] Mão; \n\n[[2]] Pé; \n\n[[3]] Mão e Pé; \n\n[[0]] sair; \n\n{rowdoble()} \n" ))
+
+            if  ops == 1:
+                listaserviços.append(unhas(ops))
+            if  ops == 2:
+                listaserviços.append(unhas(ops))
+            if  ops == 3:
+                listaserviços.append(unhas(ops))
+            if  ops == 4:
+                listaserviços.append(unhas(ops))
+            if  ops == 0:
+                break
+            break
+
+        if op == 0:
+            break
+        break
+
     return listaserviços
+
+
+
+
--- tbellohair/bloco_notas/test01.py
+++ tbellohair/bloco_notas/test01.py
@@ -1,9 +1,15 @@
 #Teste para anotar valores e serviços
-from functions import rowdoble
-
+from functions import rowdoble, tiposerv, cabelo, unhas, sombrancelha
+import pandas as pd
+import datetime
+import json
+lista_geral = []
 
 while True:
-    print("Olá,\n")
+    lista_geral.clear()
+
+    servicos = {}
+    print("\nOlá,\n")
     print(rowdoble())
     print("MENU DE OPÇÕES: ".center(60))
     print(rowdoble())
@@ -14,17 +20,27 @@ while True:
         print("'OP' Inválida!")
         op = int(input(f"\n[[1]] Novo serviço: \n\n[[2]] Vizualizar serviços realizados hoje: \n\n[[0]] Sair: \n\n{rowdoble()}\n"))
 
-    
-
     #Adcionar um novo serviço
-
-    
     if op == 1:
-        print("op 1 selecionada")
+        nome = input("Nome do cliente: \n").title()
+        servicos[nome] =tiposerv()
+        lista_geral.append(servicos)
+        data = datetime.datetime.now()
+        data_atual = data.strftime('%d/%m/%Y %H:%M:%S')
+        lista_geral.append(data_atual)
+        with open('bellohair.txt', 'a') as arquivo:
+            arquivo.write(json.dumps(lista_geral, ensure_ascii=False) + "\n")
+        
+        print(lista_geral)
+        continue
+        
     if op == 2:
-        print("op 2 selecionada")
+        with open('bellohair.txt', 'r') as arquivo:
+            db = arquivo.read()
+            print(db)
+            continue
+        
     if op == 0:
         print("good bye")
-    
     
     break
